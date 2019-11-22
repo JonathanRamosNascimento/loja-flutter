f9889fd0f917d486a480199c9e7bea977a71270c
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-22 14:36:25 -0300

    criando a pagina de categorias e iniciando a criaçao dos produtos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/tabs/home_tab.dart';
+import 'package:lojavirtual/tabs/products_tab.dart';
 import 'package:lojavirtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,8 +14,18 @@ class HomeScreen extends StatelessWidget {
       children: <Widget>[
         Scaffold(
           body: HomeTab(),
-          drawer: CustomDrawer(),
+          drawer: CustomDrawer(_pageController),
         ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+        Container(color: Colors.yellow,),
+        Container(color: Colors.green,),
       ],
     );
   }
