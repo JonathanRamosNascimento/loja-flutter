a0ca0e6068eff828ee8e15988650f58110675b68
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-22 12:42:07 -0300

    criando a home page do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,10 +7,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Flutter's Clothing",
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
       ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/tabs/home_tab.dart';
+
+class HomeScreen extends StatelessWidget {
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        HomeTab(),
+      ],
+    );
+  }
+}
