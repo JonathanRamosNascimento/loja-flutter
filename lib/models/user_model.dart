2ee06d5a9aebc4f6983f63c76ad88e78d8584d5b
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-24 21:20:59 -0300

    finalizando a criaçao do scoped model de user para o app

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,11 +1,20 @@
 import 'package:scoped_model/scoped_model.dart';
 
 class UserModel extends Model {
+
+  bool isLoading = false;
+
   void signUp() {}
 
-  void signIn() {}
+  void signIn() async {
+    isLoading = true;
+    notifyListeners();
+
+    await Future.delayed(Duration(seconds: 3));
+
+    isLoading = false;
+    notifyListeners();
+  }
 
   void recoverPass() {}
-
-  bool isLoggedIn() {}
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/models/user_model.dart';
 import 'package:lojavirtual/screens/signup_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class LoginScreen extends StatelessWidget {
-
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -20,66 +21,76 @@ class LoginScreen extends StatelessWidget {
             textColor: Colors.white,
             onPressed: () {
               Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context)=> SignUpScreen())
-              );
+                  MaterialPageRoute(builder: (context) => SignUpScreen()));
             },
           )
         ],
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(hintText: "E-mail"),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if(text.isEmpty || !text.contains("@")) return "E-mail inválido!";
-              },
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Senha"),
-              obscureText: true,
-              validator: (text) {
-                if(text.isEmpty || text.length < 6) return "Senha inválida!";
-              },
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: FlatButton(
-                onPressed: () {},
-                child: Text(
-                  "Esqueci minha senha",
-                  textAlign: TextAlign.right,
-                ),
-                padding: EdgeInsets.zero,
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              height: 44,
-              child: RaisedButton(
-                child: Text(
-                  "Entrar",
-                  style: TextStyle(fontSize: 18),
-                ),
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-                onPressed: () {
-                  if(_formKey.currentState.validate()){
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model) {
+          if (model.isLoading)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
 
-                  }
-                },
-              ),
-            )
-          ],
-        ),
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16),
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(hintText: "E-mail"),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (text) {
+                    if (text.isEmpty || !text.contains("@"))
+                      return "E-mail inválido!";
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Senha"),
+                  obscureText: true,
+                  validator: (text) {
+                    if (text.isEmpty || text.length < 6)
+                      return "Senha inválida!";
+                  },
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Esqueci minha senha",
+                      textAlign: TextAlign.right,
+                    ),
+                    padding: EdgeInsets.zero,
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    textColor: Colors.white,
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {}
+
+                      model.signIn();
+                    },
+                  ),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
