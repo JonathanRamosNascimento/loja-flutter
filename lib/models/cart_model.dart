e942240843e72804962e52d1f1e293b12afe4c0f
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-26 17:57:49 -0300

    cart

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lojavirtual/datas/cart_product.dart';
+import 'package:lojavirtual/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class CartModel extends Model {
+  UserModel user;
+
+  List<CartProduct> products = [];
+
+  CartModel(this.user);
+
+  void addCartItem(CartProduct cartProduct) {
+    products.add(cartProduct);
+
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .add(cartProduct.toMap())
+        .then((doc) {
+      cartProduct.cid = doc.documentID;
+    });
+
+    notifyListeners();
+  }
+
+  void removeCartItem(CartProduct cartProduct) {
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .document(cartProduct.cid)
+        .delete();
+
+    products.remove(cartProduct);
+
+    notifyListeners();
+  }
+}
