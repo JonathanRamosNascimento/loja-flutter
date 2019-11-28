6f1e9555ac1c6555fb2294d7784d360175b51691
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-28 16:10:38 -0300

    Mostrando item no carrinho

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lojavirtual/models/cart_model.dart';
 import 'package:lojavirtual/models/user_model.dart';
 import 'package:lojavirtual/screens/login_screen.dart';
+import 'package:lojavirtual/tiles/cart_tile.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -78,6 +79,18 @@ class CartScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,
               ),
+            );
+          } else {
+            return ListView(
+              children: <Widget>[
+                Column(
+                  children: model.products.map(
+                    (product) {
+                      return CartTile(product);
+                    }
+                  ).toList(),
+                )
+              ],
             );
           }
         },
