2f85fdfce8a3a7e8b5f9a30786e5dd3e360a0363
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-29 14:49:08 -0300

    Criando o card de frete

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -4,6 +4,7 @@ import 'package:lojavirtual/models/user_model.dart';
 import 'package:lojavirtual/screens/login_screen.dart';
 import 'package:lojavirtual/tiles/cart_tile.dart';
 import 'package:lojavirtual/widgets/discount_card.dart';
+import 'package:lojavirtual/widgets/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -91,7 +92,8 @@ class CartScreen extends StatelessWidget {
                     }
                   ).toList(),
                 ),
-                DiscountCard()
+                DiscountCard(),
+                ShipCard()
               ],
             );
           }
